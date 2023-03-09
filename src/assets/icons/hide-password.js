2dae2c875942d9ce84972fa7dfbe7fd5771e4b17
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-03-09 01:29:18 +0100

    worked on password visibility

---
+++ src/assets/icons/hide-password.js
@@ -0,0 +1,11 @@
+export const HiddenPasswordIcon = () =>{
+    return (
+        <i class="fa-solid fa-eye-low-vision password-icon"></i>
+    )
+}
+
+export const ShowPasswordIcon =() =>{
+    return(
+        <i class="fa-solid fa-eye password-icon"></i>
+    )
+}
--- src/pages/authentication/Auth.js
+++ src/pages/authentication/Auth.js
@@ -1,6 +1,21 @@
 import "./auth.css" 
+// icons
+import { HiddenPasswordIcon, ShowPasswordIcon } from "../../assets/icons/hide-password"
 
 const Auth = () => {
+    const passwordVisibility =() =>{
+        var showPassword = document.getElementById("password");
+        if (showPassword.type === "text") {
+            showPassword.type = "password"
+            document.querySelector(".hide-password").classList.toggle("eye-hidden")
+            document.querySelector(".show-password").classList.toggle("eye-visible")
+        }
+        else {
+        showPassword.type = "text"
+        document.querySelector(".hide-password").classList.toggle("eye-hidden")
+        document.querySelector(".show-password").classList.toggle("eye-visible")
+        }
+    }
   return (
     <>
         <div className='auth-container'>
@@ -16,7 +31,19 @@ const Auth = () => {
             <input
                 type="password"
                 placeholder="Password"
+                id="password"
                 />
+                <span
+                className="hide-password"
+                onClick={passwordVisibility}>
+                <HiddenPasswordIcon
+                />
+                </span>
+                <span
+                className="show-password"
+                onClick={passwordVisibility}>
+                <ShowPasswordIcon/>
+                </span>
         </div>
             <button className="auth-btn">
                 Authentication <i className="fa-solid fa-arrow-right arrow-btn"></i>
